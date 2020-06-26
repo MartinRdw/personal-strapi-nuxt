41dc59cf4c2f855d43f0ea60d7fc53ee54ef3669
Martin <martin.riedweg@gmail.com>
2020-06-26 05:42:50 +0200

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,11 @@ export default {
   */
   target: 'static',
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Martin Riedweg',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'My name is Martin, I\'m an engineering student and software developer. I’m also an indie maker that built SAAS projects.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
