35d65b9f584ceb9247f4a71720d23bf35d7297b6
Martin <martin.riedweg@gmail.com>
2020-06-28 05:53:03 +0200

    wip

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,74 @@
 <template>
   <div id="about">
-    <p>My name is Martin, I'm an engineering student and software developer.</p>
-    <p>I'm currently splitting my time between my work/study program at <a href="https://ece.fr" target="_blank">ECE Paris</a> and <a href="https://worldline.com" target="_blank">Worldline</a> and my own projects. I also do a small amount of selective freelance work mainly as Laravel/Vue.js developer.</p>
-    <p>I’m also an indie maker that built SAAS projects. My focus is currently on <a href="https://carlapp.co" target="_blank">Carl</a>.</p>
+    <div class="mb-8">
+      <p>
+        My name is Martin, I'm an engineering student and software developer.
+      </p>
+      <p>
+        I'm currently splitting my time between my work/study program at
+        <a href="https://ece.fr" target="_blank">ECE Paris</a> and
+        <a href="https://worldline.com" target="_blank">Worldline</a> and my own
+        projects. I also do a small amount of selective freelance work mainly as
+        Laravel/Vue.js developer.
+      </p>
+      <p>
+        I’m also an indie maker that built SAAS projects. My focus is currently
+        on <a href="https://carlapp.co" target="_blank">Carl</a>.
+      </p>
+    </div>
+    <div class="mt-4">
+      <p>Find me here:</p>
+      <table>
+        <tr>
+          <td class="w-20">
+            Twitter:
+          </td>
+          <td>
+            <a href="https://twitter.com/@martin_riedweg" target="_blank">@martin_riedweg</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Medium:
+          </td>
+          <td>
+            <a href="https://medium.com/@martin.riedweg" target="_blank">@martin.riedweg</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Github:
+          </td>
+          <td>
+            <a href="https://github.com/MartinRdw" target="_blank">MartinRdw</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Gitlab:
+          </td>
+          <td>
+            <a href="https://gitlab.com/martin.riedweg" target="_blank">martin.riedweg</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Linkedin:
+          </td>
+          <td>
+            <a
+              href="https://www.linkedin.com/in/martin-riedweg-725bb5109/"
+              target="_blank"
+            >Martin Riedweg</a>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import projectsQuery from '~/apollo/queries/project/projects'
+import projectsQuery from "~/apollo/queries/project/projects"
 
 export default {
   components: {},
@@ -28,11 +89,15 @@ export default {
 
 <style>
 #about {
-  @apply mt-6 text-gray-700
+  @apply mt-6 text-gray-700;
 }
 
 #about p {
-  @apply mb-4
+  @apply mb-4;
+}
+
+#about li {
+  @apply mb-1;
 }
 
 #about a {
@@ -40,6 +105,6 @@ export default {
 }
 
 #about a:hover {
-  @apply text-white bg-indigo-500
+  @apply text-white bg-indigo-500;
 }
 </style>
